fef64a8086fa28874c567ae76036ec37a7a84c0c
Joao Pedro <802.018@alunos.unigran.br>
2020-10-26 15:24:05 -0300

    Ler arquivos e escrever em JSON

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
+const { encode } = require("punycode");
 
-var conteudo;
+
 // ler o arquivo
+/*
 fs.readFile("./jooj.peep",{encoding: 'utf-8'},(erro, dados)=>{
 
     if(erro){
@@ -11,6 +13,47 @@ fs.readFile("./jooj.peep",{encoding: 'utf-8'},(erro, dados)=>{
     }
 
 });
+*/
 
 
-console.log(conteudo);
+// escrever em arquivos
+/*
+fs.writeFile("./jooj.peep","Um novo conteudo de arquivo",(err)=>{
+    if(err){
+     console.log("Erro durante o salvamento");
+}
+});
+*/
+
+//ler arquivos json
+/*
+modUsuario("joao", "C", "carro");
+
+function modUsuario(nome,curso,cat){
+fs.readFile("./usuario.json",{encoding: 'utf-8'},(erro,dados)=>{
+    if(erro){
+        console.log("Um erro aconteceu");
+    }else{
+       //console.log(dados);
+       var conteudo = JSON.parse(dados);
+
+        conteudo.nome = nome;
+        conteudo.curso = curso;
+        conteudo.cat = cat;
+
+        fs.writeFile("./usuario.json",JSON.stringify(conteudo),(erro)=>{
+            
+            if(erro){
+                console.log("Um erro aconteceu durante a escrita");
+              }  
+        });
+
+       //console.log(conteudo);
+    }
+});
+}
+*/
+
+
+
+
